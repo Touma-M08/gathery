8b795e3736051345f472511ca35325e7f904b5a8
Touma-M08 <touma_064810@yahoo.co.jp>
2022-08-14 20:39:07 +0900

    行きたい！リストの表示作成

--- app/Want.php
+++ app/Want.php
@@ -3,12 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Want extends Model
 {
     protected $fillable = [
         'user_id', 'place_id'
     ];
+    
+    public function getByWantPlace(int $limit_count = 5)
+    {
+        return $this::with('place','user')->where('user_id', Auth::user()->id)->get();
+    }
     
     public function user() {
         return $this->belongsTo('App\User');
